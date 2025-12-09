cc36555c52ddef010766f6bf3779d55d98819693
Pawan Sahu <pawan@Pawans-MacBook-Pro.local>
2025-12-09 23:02:47 +0530

    code added

--- packages/server-api/index.js
+++ packages/server-api/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend server");
+  res.send("Backend server from develop branch");
 });
 app.listen(port, (err) => {
   if (err) {
